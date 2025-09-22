aa3e30df405815f58e6eb63506fa023125486fb6
Niamh Brown <niamh.brown@vet-ct.com>
2025-09-22 18:19:52 +0100

    add log form is inside dialog. basic functionality done.

--- frontend/src/components/exercise-library/AddLogDialog.tsx
+++ frontend/src/components/exercise-library/AddLogDialog.tsx
@@ -7,8 +7,65 @@ import {
   DialogTrigger,
 } from "@radix-ui/react-dialog";
 import { DialogFooter, DialogHeader } from "../ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useFieldArray, useForm, type Resolver } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { useAddLog } from "@/hooks/use-addLogs";
+import { Trash2 } from "lucide-react";
 
-export const AddLogDialog = () => {
+interface Props {
+  exerciseId: string;
+}
+const logFormSchema = z.object({
+  logs: z.array(
+    z.object({
+      reps: z.coerce.number().min(1, "must be more than 0 reps"),
+      weight: z.coerce.number().min(1, "must be more than 0 kg"),
+    })
+  ),
+});
+export const AddLogDialog = ({ exerciseId }: Props) => {
+  const addLogMutation = useAddLog();
+  const form = useForm<z.infer<typeof logFormSchema>>({
+    resolver: zodResolver(logFormSchema) as Resolver<
+      z.infer<typeof logFormSchema>
+    >, // this fixed the coerce errors
+    defaultValues: {
+      logs: [
+        { reps: 0, weight: 0 },
+        { reps: 0, weight: 0 },
+        { reps: 0, weight: 0 },
+      ],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "logs",
+  });
+  const onSubmit = (data: z.infer<typeof logFormSchema>) => {
+    console.log("✅", data);
+    addLogMutation.mutate({
+      exerciseId,
+      logs: data.logs, // send the entire array at once
+    });
+  };
+
+  // const formMessage = () => {
+  //   if (addLogMutation.isPending) return <p>Saving log...</p>;
+  //   if (addLogMutation.isError)
+  //     return <p>Error saving log: {addLogMutation.error?.message}</p>;
+  //   if (addLogMutation.isSuccess) return <p>Log saved!</p>;
+  // }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -18,12 +75,75 @@ export const AddLogDialog = () => {
         <DialogHeader>
           <DialogTitle>Add Exercise Log</DialogTitle>
         </DialogHeader>
-        <p>HELLO, my form will eventually go here</p>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            id="log-form"
+            className="space-y-4"
+          >
+            {fields.map((field, index) => (
+              <div
+                key={field.id}
+                className="grid grid-cols-3 gap-2 items-center"
+              >
+                <span>Set {index + 1}</span>
+
+                <FormField
+                  control={form.control}
+                  name={`logs.${index}.reps`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Reps</FormLabel>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name={`logs.${index}.weight`}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Weight</FormLabel>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {fields.length - 1 === index && index !== 0 && (
+                  <Button
+                    type="button"
+                    variant="outline"
+                    onClick={() => remove(index)}
+                  >
+                    <Trash2 />
+                  </Button>
+                )}
+              </div>
+            ))}
+
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={() => append({ reps: 0, weight: 0 })}
+            >
+              Add Row
+            </Button>
+          </form>
+        </Form>
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
           </DialogClose>
-          <Button type="submit">Save</Button>
+          <Button type="submit" form="log-form">
+            Save
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- frontend/src/components/exercise-library/AddLogsForm.tsx
+++ frontend/src/components/exercise-library/AddLogsForm.tsx
@@ -1,101 +1,117 @@
-"use client";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useFieldArray, useForm, type Resolver } from "react-hook-form";
+// import { z } from "zod";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, type Resolver } from "react-hook-form";
-import { z } from "zod";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { useAddLog } from "@/hooks/use-addLogs";
 
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { useAddLog } from "@/hooks/use-addLogs";
+// const logFormSchema = z.object({
+//   logs: z.array(
+//     z.object({
+//       reps: z.coerce.number(),
+//       weight: z.coerce.number().min(0, "Weight must be 0 or more"),
+//     })
+//   ),
+// });
+// interface Props {
+//   exerciseId: string;
+// }
+// export const AddLogsForm = ({ exerciseId }: Props) => {
+//   const addLogMutation = useAddLog();
+//   const form = useForm<z.infer<typeof logFormSchema>>({
+//     resolver: zodResolver(logFormSchema) as Resolver<
+//       z.infer<typeof logFormSchema>
+//     >, // this fixed the coerce errors
+//     defaultValues: {
+//       logs: [
+//         { reps: 0, weight: 0 },
+//         { reps: 0, weight: 0 },
+//         { reps: 0, weight: 0 },
+//       ],
+//     },
+//   });
+//   const { fields, append, remove } = useFieldArray({
+//     control: form.control,
+//     name: "logs",
+//   });
+//   const onSubmit = (data: z.infer<typeof logFormSchema>) => {
+//     console.log("✅", data);
+//     addLogMutation.mutate({
+//       exerciseId,
+//       logs: data.logs, // send the entire array at once
+//     });
+//   };
 
-const logFormSchema = z.object({
-  logs: z.array(
-    z.object({
-      reps: z.coerce.number(),
-      weight: z.coerce.number().min(0, "Weight must be 0 or more"),
-    })
-  ),
-});
-interface Props {
-  exerciseId: string;
-}
-// change function type and name
-export const AddLogsForm = ({ exerciseId }: Props) => {
-  const addLogMutation = useAddLog();
-  const form = useForm<z.infer<typeof logFormSchema>>({
-    resolver: zodResolver(logFormSchema) as Resolver<
-      z.infer<typeof logFormSchema>
-    >, // this fixed the coerce errors
-    defaultValues: {
-      logs: [
-        { reps: 0, weight: 0 },
-        { reps: 0, weight: 0 },
-        { reps: 0, weight: 0 },
-      ],
-    },
-  });
+//   if (addLogMutation.isPending) return <p>Saving log...</p>;
+//   if (addLogMutation.isError)
+//     return <p>Error saving log: {addLogMutation.error?.message}</p>;
+//   if (addLogMutation.isSuccess) return <p>Log saved!</p>;
 
-  const onSubmit = (data: z.infer<typeof logFormSchema>) => {
-    console.log("✅", data);
-    addLogMutation.mutate({
-      exerciseId,
-      logs: data.logs, // send the entire array at once
-    });
-  };
+//   return (
+//     <Form {...form}>
+//       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+//         {fields.map((field, index) => (
+//           <div key={field.id} className="grid grid-cols-3 gap-2 items-center">
+//             <span>Set {index + 1}</span>
 
-  if (addLogMutation.isPending) return <p>Saving log...</p>;
-  if (addLogMutation.isError)
-    return <p>Error saving log: {addLogMutation.error?.message}</p>;
-  if (addLogMutation.isSuccess) return <p>Log saved!</p>;
+//             <FormField
+//               control={form.control}
+//               name={`logs.${index}.reps`}
+//               render={({ field }) => (
+//                 <FormItem>
+//                   <FormLabel>Reps</FormLabel>
+//                   <FormControl>
+//                     <Input type="number" {...field} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        {form.watch("logs").map((_, index) => (
-          <div key={index} className="grid grid-cols-3 gap-4 items-center">
-            {/* Hardcoded set number */}
-            <span>Set {index + 1}</span>
+//             <FormField
+//               control={form.control}
+//               name={`logs.${index}.weight`}
+//               render={({ field }) => (
+//                 <FormItem>
+//                   <FormLabel>Weight</FormLabel>
+//                   <FormControl>
+//                     <Input type="number" {...field} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//             {fields.length - 1 === index && index !== 0 && (
+//               <Button
+//                 type="button"
+//                 variant="outline"
+//                 onClick={() => remove(index)}
+//               >
+//                 Remove
+//               </Button>
+//             )}
+//           </div>
+//         ))}
 
-            <FormField
-              control={form.control}
-              name={`logs.${index}.reps`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Reps</FormLabel>
-                  <FormControl>
-                    <Input type="number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+//         <Button
+//           type="button"
+//           variant="secondary"
+//           onClick={() => append({ reps: 0, weight: 0 })}
+//         >
+//           Add Row
+//         </Button>
 
-            <FormField
-              control={form.control}
-              name={`logs.${index}.weight`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Weight</FormLabel>
-                  <FormControl>
-                    <Input type="number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-        ))}
-
-        <Button type="submit">Save Logs</Button>
-      </form>
-    </Form>
-  );
-};
+//         <Button type="submit">Submit</Button>
+//       </form>
+//     </Form>
+//   );
+// };
